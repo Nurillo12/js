05fb021abccddab759b2aa9108a0fb39593a2e2c
Nurillo <isroilovnurillo2001@.com>
2023-02-02 14:26:16 +0500

    ok

---
+++ script1.js
@@ -0,0 +1,85 @@
+let age = 1000    //data-type number
+console.log(age)
+
+let myName = 'Chalie'   //data-type script
+console.log(myName)
+
+let bool = true    //data-type boolen
+console.log(bool)
+
+let number;        //data-type undefined
+console.log(number)
+
+//console.log(hdcfgvjkvdio)  //data-type null
+
+//object
+
+let person = {
+    age: 21,
+    name1: 'Steven',
+    isMarried: false,
+}
+
+console.log(person)
+
+let arr = [
+    'Laptop', 'Phone', 'Mouse', 'Keyboard', 'Glasses'
+]
+
+arr[1]='Software'
+ 
+console.log(arr[3])
+
+let x=10
+let y=20
+
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+
+let s=100
+console.log(s % 7)
+
+x=6
+x=x+4
+console.log(x)
+
+console.log(2==0)
+
+console.log(2===2)
+console.log(2/0)
+
+console.log('2'+0)
+//alert('okay?')
+
+alert('hi guys')
+
+alert('okni bosing gazini!')
+
+alert('Mazgiiiiii!!!')
+alert('nima gap guys')
+alert('ihdbfvjbhgbfjhb')
+
+let answers = confirm('ijklmopqrst')
+console.log(answers)
+
+let answer=prompt('lalulla')
+console.log(answer)
+alert('vay bleeee')
+alert('okni bosormeng eeee')
+alert('obbboooo')
+
+alert('hit qivordizu')
+alert('yana ok ni bosdi eee')
+alert('tugadiii!!!')
+alert('tugadi dedimu nimaga yana bosyapsz?')
+alert('vaay bleeeee')
+alert('Maaaaaazgiiiiii!!!')
+alert('boldiyee!')
+alert('bosormang ashi okni')
+
+
+
+
+
